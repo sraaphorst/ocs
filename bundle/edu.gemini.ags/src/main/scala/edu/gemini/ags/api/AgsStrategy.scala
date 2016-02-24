2225a1401aaf1fe250c37b276f59db4a8cbb9c6e
Sebastian Raaphorst <sraaphor@gemini.edu>
2016-02-24 16:06:20 -0300

    OCSADV-519: Added hand-off object processing to OtItemEditor init method. WIP.

--- bundle/edu.gemini.ags/src/main/scala/edu/gemini/ags/api/AgsStrategy.scala
+++ bundle/edu.gemini.ags/src/main/scala/edu/gemini/ags/api/AgsStrategy.scala
@@ -94,18 +94,18 @@ object AgsStrategy {
 
       // If this is different from the old automatic GG, then replace.
       val oldGuideEnvironment = env.getGuideEnvironment
-      val oldGuideEnv         = oldGuideEnvironment.guideEnv
+      val oldGuideEnv = oldGuideEnvironment.guideEnv
 
       // SPTargets are compared by references, so we extract the names and compare.
       def extractNames(auto: AutomaticGroup) = auto.targetMap.map(_.getName)
 
-      if (extractNames(oldGuideEnv.auto) =/= extractNames(newAuto)) {
-        val newGuideEnv = oldGuideEnv.copy(auto = newAuto)
-        val newGuideEnvironment = oldGuideEnvironment.copy(guideEnv = newGuideEnv)
-        env.setGuideEnvironment(newGuideEnvironment)
-      } else {
-        env
-      }
+      //if (extractNames(oldGuideEnv.auto) =/= extractNames(newAuto)) {
+      val newGuideEnv = oldGuideEnv.copy(auto = newAuto)
+      val newGuideEnvironment = oldGuideEnvironment.copy(guideEnv = newGuideEnv)
+      env.setGuideEnvironment(newGuideEnvironment)
+      //} else {
+      //        env
+      //      }
     }
 
     def applyTo(ctx: ObsContext): ObsContext = {
